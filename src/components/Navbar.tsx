476003d053c2330d8e7abfb7f14983309868230d
IvanStukalov <ivanstukalov2003@gmail.com>
2024-01-22 13:25:48 +0300

    fixes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,29 +10,33 @@ interface Props {
 const NavBar: React.FC<Props> = ({ path, slug }) => {
   return (
     <Navbar className="navbar">
-      <div className='divider'></div>
+      <>
+        <div className='divider'></div>
 
-      <Link to="/StarEvents-ReactApp/" className="breadcrump__item breadcrump__part">Звезды</Link>
+        <Link to="/StarEvents-ReactApp/" className="breadcrump__item breadcrump__part">Звезды</Link>
 
-      {slug ?
-        <Link to={`/StarEvents-ReactApp/star/${path}`} className="breadcrump__item">
-          <div className="breadcrump__part"> -&gt; </div>
-          <div className="breadcrump__part">{slug}</div>
+        {slug ?
+          <Link to={`/StarEvents-ReactApp/star/${path}`} className="breadcrump__item">
+            <div className="breadcrump__part"> -&gt; </div>
+            <div className="breadcrump__part">{slug}</div>
+          </Link>
+          : path &&
+          <Link to={`/StarEvents-ReactApp/${path}`} className="breadcrump__item">
+            <div className="breadcrump__part"> -&gt; </div>
+            <div className="breadcrump__part">{path}</div>
+          </Link>
+        }
+      </>
+
+      <>
+        <Link className="navbar_item navbar-btn" to="/auth">
+          <Button variant="primary">Войти</Button>
         </Link>
-        : path &&
-        <Link to={`/StarEvents-ReactApp/${path}`} className="breadcrump__item">
-          <div className="breadcrump__part"> -&gt; </div>
-          <div className="breadcrump__part">{path}</div>
+
+        <Link className="navbar_item navbar-btn" to="/reg">
+          <Button variant="secondary">Регистрация</Button>
         </Link>
-      }
-
-      <Link className="navbar_item navbar-btn" to="/auth">
-        <Button variant="primary">Войти</Button>
-      </Link>
-
-      <Link className="navbar_item navbar-btn" to="/reg">
-        <Button variant="secondary">Регистрация</Button>
-      </Link>
+      </>
     </Navbar>
   );
 }
